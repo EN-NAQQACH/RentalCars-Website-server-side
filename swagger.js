86a9059358a4942474f3c66a8eed9fdfbb5f2432
EN-NAQQACH <ennaqqach.contact@gmail.com>
2024-04-15 21:05:39 +0100

    swagger

---
+++ swagger.js
@@ -0,0 +1,24 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Quiz API',
+            version: '1.3.1',
+            description: 'API for the Quiz app',
+        },
+    },
+    apis: ["./server/routes/*.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+const serve = swaggerUi.serve;
+const setup = swaggerUi.setup(swaggerSpec);
+
+export default {
+    serve,
+    setup
+};
