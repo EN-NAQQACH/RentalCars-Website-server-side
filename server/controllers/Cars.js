1eec5b71a5c05713ad4a65bfac9b1452a2128805
EN-NAQQACH <ennaqqach.contact@gmail.com>
2024-05-07 02:24:16 +0100

    controllers

--- server/controllers/Cars.js
+++ server/controllers/Cars.js
@@ -5,37 +5,76 @@ import dotenv from 'dotenv';
 dotenv.config();
 import multer from 'multer';
 
+// async function AddCar(req, res) {
+//     try {
+//         const { location, model, year, make, price, description, distance, transmission, maxtrip, mintrip,photos,features, carseat} = req.body;
+//         const token = req.headers.authorization.split(' ')[1];
+//         const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+//         const userId = decoded.id;
+//         const car = await prisma.car.create({
+//             data: {
+//                 location: location,
+//                 model: model,
+//                 make: make,
+//                 features: features,
+//                 year: year,
+//                 price: price,
+//                 description: description,
+//                 distance: distance,
+//                 transmission: transmission,
+//                 maxTrip: maxtrip,
+//                 minTrip: mintrip,
+//                 carSeats: carseat,
+//                 imageUrls:photos,
+//                 userId: userId,
+//             },
+//         });
+//         if (!car) {
+//             return res.status(400).json({ error: "Car not added" });
+//         }
+//         res.status(200).json({ message: "Car added" });
+//     } catch (error) {
+//         if (error.name === 'JsonWebTokenError') {
+//             return res.status(401).json({ error: "Unauthorized" });
+//         }else if(error.name === '_TokenExpiredError'){
+//             return res.status(401).json({ error: "token expired" });
+//         } else {
+//             console.error(error);
+//             res.status(500).json({ error: 'Server Error' });
+//         }
+//     }
+// }
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'uploads/'); // Specify the directory where uploaded files will be stored
+        cb(null, 'uploads/');
     },
     filename: function (req, file, cb) {
-      cb(null, Date.now() + '-' + file.originalname); // Define the filename for the uploaded file
+        cb(null, Date.now() + '-' + file.originalname);
     }
-  });
-  
-  // Initialize multer instance
-  const upload = multer({ storage: storage });
+});
+const upload = multer({ storage: storage });
 
-async function AddCar(req, res) {
+async function AddCar(req,res){
     try {
-
-        const { location, model, year, make, price, description, distance, transmission, maxtrip, mintrip,photos, features, carseat} = req.body;
-
+        const { location,fuel, model, year, make, price, description, distance, transmission, maxtrip, mintrip, features, type, carseat } = req.body;
         const token = req.headers.authorization.split(' ')[1];
         const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
         const userId = decoded.id;
+        const imageUrls = req.files.map(file => "http://localhost:5600/" + file.path);
         const car = await prisma.car.create({
             data: {
                 location: location,
                 model: model,
                 make: make,
+                fuel:fuel,
+                Type:type,
                 features: features,
                 year: parseInt(year),
                 price: parseFloat(price),
-                imageUrls:photos,
                 description: description,
                 distance: distance,
+                imageUrls: imageUrls,
                 transmission: transmission,
                 maxTrip: parseInt(maxtrip),
                 minTrip: parseInt(mintrip),
@@ -50,8 +89,6 @@ async function AddCar(req, res) {
     } catch (error) {
         if (error.name === 'JsonWebTokenError') {
             return res.status(401).json({ error: "Unauthorized" });
-        }else if(error.name === '_TokenExpiredError'){
-            return res.status(401).json({ error: "token expired" });
         } else {
             console.error(error);
             res.status(500).json({ error: 'Server Error' });
@@ -131,9 +168,12 @@ async function GetAllCars(req, res) {
 }
 async function GetCarsUser(req, res) {
     try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const userId = decoded.id;
         const car = await prisma.car.findMany({
             where: {
-                userId:req.params.id,
+                userId: userId,
             },
         });
         if (!car) {
@@ -141,15 +181,22 @@ async function GetCarsUser(req, res) {
         }
         res.status(200).json(car);
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Server Error' });
+        if (error.name === 'JsonWebTokenError') {
+            return res.status(401).json({ error: "Unauthorized" });
+        } else {
+            console.error(error);
+            res.status(500).json({ error: 'Server Error' });
+        }
     }
 }
 async function GetaUserCar(req,res){
     try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const userId = decoded.id;
         const car = await prisma.car.findUnique({
             where: {
-                userId:req.params.userId,
+                userId:userId,
                 id:req.params.carId,
             },
         });
@@ -158,9 +205,13 @@ async function GetaUserCar(req,res){
         }
         res.status(200).json(car);
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Server Error' });
+        if (error.name === 'JsonWebTokenError') {
+            return res.status(401).json({ error: "Unauthorized" });
+        } else {
+            console.error(error);
+            res.status(500).json({ error: 'Server Error' });
+        }
     }
     
 }
-export { AddCar, upload };
+export { AddCar,upload,GetCarsUser,GetaUserCar};
