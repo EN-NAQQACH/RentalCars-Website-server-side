2df9321dce322b0e4d8d0c0768cb94f5a285e1df
EN-NAQQACH <ennaqqach.contact@gmail.com>
2024-05-11 02:07:21 +0100

    GetCars by destination / by make (routes)

--- server/routes/cars.js
+++ server/routes/cars.js
@@ -7,7 +7,7 @@ import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv';
 dotenv.config();
 import multer from 'multer';
-import { AddCar,upload,GetCarsUser,GetaUserCarUnauth,GetCarAuth,UpdateCar,GetAllCars,GetAllCarsUnauth,DeleteCars} from '../controllers/cars.js';
+import { AddCar,upload,GetCarsUser,GetaUserCarUnauth,GetCarAuth,UpdateCar,GetAllCars,GetAllCarsUnauth,DeleteCars,GetAllCarsByMakeAuth,GetAllCarsByMakeunAuth,GetAllCarsByDestinataionAuth,GetAllCarsByDestinataionunAuth} from '../controllers/cars.js';
 
 router.put('/updatecar/:carId',upload.array('photos'), UpdateCar);
 router.post('/addcar', upload.array('photos'), AddCar);
@@ -17,5 +17,9 @@ router.get('/getcarunauth/:carId', GetaUserCarUnauth);
 router.get('/getallcars',GetAllCars);
 router.get('/getallcarsunauth', GetAllCarsUnauth);
 router.delete('/delete/car/:carId',DeleteCars);
+router.get('/getallcarsbymake/', GetAllCarsByMakeAuth);
+router.get('/getallcarsbymakeunauth/', GetAllCarsByMakeunAuth);
+router.get('/getallcarsbydestination/', GetAllCarsByDestinataionAuth);
+router.get('/getallcarsbydestinationunauth/', GetAllCarsByDestinataionunAuth);
 // router.post('/addcar',AddCar);
 export default router;
