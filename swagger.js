26ab73d8edc66bd43fc0e25e952b22e73e886b20
EN-NAQQACH <ennaqqach.contact@gmail.com>
2024-05-21 08:17:21 +0100

    test

--- swagger.js
+++ swagger.js
@@ -1,7 +1,7 @@
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
+
 const options = {
     definition: {
         openapi: '3.0.0',
@@ -18,6 +18,8 @@ const swaggerSpec = swaggerJSDoc(options);
 
 const serve = swaggerUi.serve;
 const setup = swaggerUi.setup(swaggerSpec, {
+    customCss:
+        '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
     customCssUrl: CSS_URL,
 });
 
