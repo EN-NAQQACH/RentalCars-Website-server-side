e87850cf2d60befb9cc13fefb669e240d4635bbd
EN-NAQQACH <ennaqqach.contact@gmail.com>
2024-05-27 19:49:44 +0100

    Server

--- index.js
+++ index.js
@@ -16,7 +16,44 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api-docs/api', swagger.serve, swagger.setup);
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>EaslyCars</title>
+      <style>
+          body {
+              font-family: Arial, sans-serif;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              height: 100vh;
+              margin: 0;
+              background-color: #f4f4f4;
+          }
+          .message-container {
+              text-align: center;
+              background-color: #fff;
+              padding: 20px;
+              border-radius: 10px;
+              box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+          }
+          .message-container h1 {
+              margin: 0;
+              font-size: 24px;
+              color: #333;
+          }
+      </style>
+  </head>
+  <body>
+      <div class="message-container">
+          <h1>EaslyCars Server</h1>
+      </div>
+  </body>
+  </html>
+`);
 })
 
 app.use("/api", users);
